8baa913d0a253e4c70b7d3f838e01924cbb58a68
Dan Andrei <andreidani96@gmail.com>
2015-08-18 10:22:27 +0300

    Added basic validation functionality

--- index.js
+++ index.js
@@ -1,3 +1,41 @@
-exports.init = function () {
-	var self = this;
+var tv4 = require("./tv4");
+
+exports.validate = function (data) {
+    var self = this;
+
+    // check data
+    if (!data || !data.data || !data.schema) {
+        return console.error("Data provided not valid");
+    }
+
+    // check if the required schema was configured
+    if (!self._config || !self._config.schema || !self._config.schema[data.schema]) {
+        return console.error("Schema not configured");
+    }
+
+    // look for a callback
+    var callback = data.callback || function () {};
+
+    // validate data
+    var schema = self._config.schema[data.schema];
+    var valid = tv4.validate(data.data, schema);
+
+    if (valid) {
+        self.flow("schema_valid").write(null, {
+            data: data.data,
+            schema: data.schema
+        });
+
+        // send response back via callback
+        callback(true);
+    } else {
+        var error = tv4.error;
+        self.flow("schema_invalid").write(null, {
+            schema: data.schema,
+            error: error
+        });
+
+        // send response back via callback
+        callback(false, error);
+    }
 };
